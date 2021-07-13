d6fb5e9a9030c7bce0883fddae7d669c083047b3
gars150387 <gar.santeliz@gmail.com>
2021-07-13 21:02:33 +0000

    right props in card

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,20 +1,16 @@
 import PropTypes from "prop-types";
 import React from "react";
 
-const card = ({ number }) => {
+const card = prop => {
 	return (
 		<div
 			className="card text-white bg-dark mb-3"
 			style={{ maxWidth: "18rem" }}>
 			<div className="card-body">
-				<p className="card-text">{number}</p>
+				<p className="card-text">{prop.number}</p>
 			</div>
 		</div>
 	);
-};
-
-card.propTypes = {
-	number: PropTypes.number
 };
 
 export default card;
